9bd77bde3891af479bd005c1748d0a4fb7527409
AngelPuent3 <99633191+AngelPuent3@users.noreply.github.com>
2022-03-18 22:30:53 -0600

    Delete Proyecto base de datos policia/BaseDeDatos.py directory

--- Proyecto base de datos policia/BaseDeDatos.py/Atraco.py
+++
@@ -1,8 +0,0 @@
-class Atraco:
-
-    def listAtraco(self):
-        self.fechaAtraco=str(input("Ingrese la fecha del atraco: \n Ejemplo: (31/Septiembre/2027) \n >"))
-        return(self.Atraco)
-
-
-
