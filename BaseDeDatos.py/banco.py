6b94072085667113ff2e154242956b343b2e79ec
AngelPuent3 <99633191+AngelPuent3@users.noreply.github.com>
2022-03-22 09:04:52 -0600

    Update and rename Banco.py to banco.py

--- BaseDeDatos.py/banco.py
+++ BaseDeDatos.py/banco.py
@@ -1,23 +1,50 @@
-from main import mainP
-from Sucursal import Sucursal
-from Vigilante import Vigilante
-global listSBanco
+global listBanco
+global listContrato
+listBanco=list()
+listContrato=list()
 
 class Banco:
-     def listaSBanco(self):
-          listSBanco = []
+
+    def mainBanco(self):
+        from sucursal import Sucursal
+        from main import mainP
+        print("Menu Banco")
+        print("---------")
+        print(
+            "Añadir Banco\n, 2.- Visualizar Banco\n 3.Agregar Sucursales\n  4.- Contratar Vigilante 5.Regresar a menu principal\n ")
+        opMb = (int(input(">")))
+        if opMb == 1:
+            mainb = Banco()
+            mainb.listaSBanco()
+        elif opMb == 2:
+            mainb = Banco()
+            mainb.visualizarBanco()
+        elif opMb == 3:
+            suc = Sucursal()
+            suc.mainSucursal()
+        elif opMb == 4:
+            mainb = Banco()
+            mainb.contratarBanco()
+        elif opMb == 5:
+            mainP()
+        else:
+            exit("Adios")
+
+
+    def listaSBanco(self):
           b=Banco()
           opBa = int(input("¿Cuantos sucursales  tiene el banco?"))
           for i in range(opBa):
                b.codigoBanco = int(input("Ingrese la clave de la sucursal\n>"))
                b.edadBanco = int(input("Ingrese los años de la sucursal\n>"))
-               listSBanco.append(listSBanco)
+               b.domicilioBanco=str(input("Ingrese la direccion del banco\nEjem: 14 norte Col. Centro"))
+               listBanco.append(b)
           b.mainBanco()
 
-     def visualizarBanco(self):
+    def visualizarBanco(self):
           b=Banco()
-          for b in listSBanco:
-               print("**Registro Juez*")
+          for b in listBanco:
+               print("**Registro banco*")
                print("-------------")
                print("CodigoBanco:", b.codigoBanco, "\nDomicilio del banco:", b.domicilioBanco,
                      "\n Sucursales banco:",
@@ -25,7 +52,8 @@ class Banco:
                print("-------------")
           b.mainBanco()
 
-     def contratarBanco(self):
+    def contratarBanco(self):
+          from vigilante import Vigilante
           viCon = Vigilante()
           viCon.mainVigilante()
           print("¿Desea contrarta al vigilante?\n"
@@ -45,35 +73,6 @@ class Banco:
                b.mainBanco()
           else:
                print("Error")
-
-
-     def mainBanco(self):
-          print("Menu Banco")
-          print("---------")
-          print(
-               "Añadir Banco\n, 2.- Visualizar Banco\n 3.Agregar Sucursales\n  4.- Contratar Vigilante 5.Regresar a menu principal\n ")
-          opMb= (int(input(">")))
-          if opMb == 1:
-               mainb = Banco()
-               mainb.listaSBanco()
-          elif opMb == 2:
-               mainb = Banco()
-               mainb.visualizarBanco()
-          elif opMb == 3:
-               suc = Sucursal()
-               suc.mainSucursal()
-          elif opMb==4:
-               mainb=Banco()
-               mainb.contratarBanco()
-          elif opMb ==5:
-               mainP()
-          else :
-               print("error loa regraremos al menu principal \n ", mainP())
-
-
-
-
-
 
 
 
